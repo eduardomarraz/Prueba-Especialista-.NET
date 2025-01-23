9280b24ddf40455dc1094fd3a3cabd6ee6e21df0
Eduardo <eduardomr_@hotmail.com>
2025-01-23 17:43:12 +0100

    Agregar archivos de proyecto.

---
+++ Prueba Especialista .NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prueba_Especialista_.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
